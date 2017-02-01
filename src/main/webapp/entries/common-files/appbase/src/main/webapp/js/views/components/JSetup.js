3fb574f082cb61b17b8ef40a4a6d7aa4169c2979
Robson <robsonmrsp@gmail.com>
2017-02-01 14:27:57 -0200

    Criação do JSetupExpandableCell

--- src/main/webapp/entries/common-files/appbase/src/main/webapp/js/views/components/JSetup.js
+++ src/main/webapp/entries/common-files/appbase/src/main/webapp/js/views/components/JSetup.js
@@ -16,44 +16,6 @@ define(function(require) {
 	var InputUpload = require('views/components/InputUpload');
 	var Suggestbox = require('views/components/Suggestbox');
 
-	var View = Marionette.LayoutView.extend({
-		onShow : function() {
-			$.validate({
-				modules : 'location, date, security, brazil',
-				validateOnEvent : true,
-				inputParentClassOnSuccess : '',
-				addValidClassOnAll : true,
-			});
-
-			this.onShowView && this.onShowView();
-		},
-
-		isValid : function() {
-			// checar se realmente é necessário
-			return this.$el.isValid(null, {
-				modules : 'location, date, security, brazil',
-				validateOnEvent : true,
-				inputParentClassOnSuccess : '',
-				addValidClassOnAll : true,
-			});
-		},
-
-		clearForm : function() {
-			if (this.customClearForm) {
-				this.customClearForm();
-			}
-			if (this.ui) {
-				_.each(this.ui, function(uiItem) {
-					if (!uiItem.attr('persist'))
-						util.clear(uiItem.attr('id'));
-				});
-			}
-			// TODO ver uma forma mais elegente de limpar os campos de upload;
-			var inputImage = $('.jsetup-upload-image');
-			inputImage.attr('src', inputImage.attr('no-image-file'));
-		}
-	});
-
 	var Counter = Marionette.ItemView.extend({
 
 		template : _.template(CounterTemplate),
@@ -194,6 +156,91 @@ define(function(require) {
 		},
 	})
 
+	//adaptado de https://github.com/cime/backgrid-expandable-cell
+	var ExpandableCell = Backgrid.Cell.extend({
+		className : "expand-col",
+		accordion : true,
+		toggle : '<i style="cursor: pointer;" class="glyphicon toggle "></i>',
+		toggleClass : 'toggle',
+		toggleExpandedClass : 'glyphicon-minus-sign',
+		toggleCollapsedClass : 'glyphicon-plus-sign',
+		trClass : 'expandable',
+		tdClass : 'expandable-content',
+
+		events : {
+			'click' : 'setToggle'
+		},
+		initialize : function(options) {
+			if (options.accordion) {
+				this.accordion = options.accordion;
+			}
+
+			this.column = options.column;
+			if (!(this.column instanceof Backgrid.Column)) {
+				this.column = new Backgrid.Column(this.column);
+			}
+
+			var column = this.column;
+			var model = this.model;
+			var $el = this.$el;
+
+			if (Backgrid.callByNeed(column.renderable(), column, model))
+				$el.addClass("renderable");
+		},
+
+		render : function() {
+			/* follow along with the original render really... */
+			this.$el.empty();
+
+			this.$toggleEl = $(this.toggle).addClass(this.toggleClass).addClass(this.toggleCollapsedClass);
+
+			this.$el.append(this.$toggleEl);
+
+			this.delegateEvents();
+
+			return this;
+		},
+
+		setToggle : function() {
+			var detailsRow = this.$el.data('details');
+			var toggle = this.$toggleEl;
+
+			/* if there's details data already stored, then we'll remove it */
+			if (detailsRow) {
+				$(detailsRow).remove();
+				this.$el.data('details', null);
+				toggle.removeClass(this.toggleExpandedClass).addClass(this.toggleCollapsedClass);
+			} else {
+				if (this.accordion) {
+					var table = this.$el.closest('table');
+					$('.' + this.toggleClass, table).filter('.' + this.toggleExpandedClass).click();
+				}
+
+				var renderableColumns = this.$el.closest('table').find('th.renderable').length;
+				var isRenderable = false;
+				var cellClass = this.tdClass;
+
+				if (Backgrid.callByNeed(this.column.renderable(), this.column, this.model)) {
+					isRenderable = true;
+					cellClass += ' renderable';
+				}
+
+				/* build a jquery object for the new row... */
+				detailsRow = $('<tr class="' + this.trClass + '"><td class="' + (isRenderable ? 'renderable' : '') + '"></td><td class="' + cellClass + '" colspan="' + (renderableColumns - 1) + '"></td></tr>');
+
+				/* Inject new row */
+				this.$el.closest('tr').after(detailsRow);
+
+				/* Call expand function */
+				this.column.get('expand')(detailsRow.find('td.' + this.tdClass), this.model);
+
+				this.$el.data('details', detailsRow);
+
+				toggle.removeClass(this.toggleCollapsedClass).addClass(this.toggleExpandedClass);
+			}
+		}
+	});
+
 	var NumericCell = Backgrid.Cell.extend({
 		className : "custom-number-cel",
 		type : 'decimal',
@@ -317,6 +364,8 @@ define(function(require) {
 
 		ActionCell : GeneralCell,
 
+		ExpandableCell : ExpandableCell,
+
 		EntityCell : EntityCell,
 
 		DataTable : DataTable,
@@ -326,7 +375,6 @@ define(function(require) {
 
 		BaseModel : BaseModel,
 		BaseCollection : BaseCollection,
-		View : View,
 
 	}
 	return JSetup;
