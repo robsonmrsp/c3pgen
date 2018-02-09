c082a279b4d7fb27a4ca83bb85ec72935774fb1b
Robson <robsonmrsp@gmail.com>
2018-02-09 14:34:19 -0200

    Alteração para funcionamento do SPA externo

--- src/main/webapp/entries/common-files/appbase/src/main/java/br/com/app/config/SpringSecurityConfig.java
+++ src/main/webapp/entries/common-files/appbase/src/main/java/br/com/app/config/SpringSecurityConfig.java
@@ -2,6 +2,9 @@ package ${application.rootPackage}.config;
 
 import javax.inject.Inject;
 
+
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,40 +18,62 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Inject
-	UserDetailsService userDetailsService;
+    @Inject
+    UserDetailsService userDetailsService;
 
-	@Inject
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder(11));
-	}
+    @Inject
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder(11));
+    }
+    
+    
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-	    http
-	    	.authorizeRequests()
-	    		.antMatchers("/js/**", "/css/**", "/images/**", "fonts/**").permitAll()
-	    		.antMatchers("/index.jsp").fullyAuthenticated()
-	    		.antMatchers("/").fullyAuthenticated()
-	            .and()
-	            .formLogin()
-	            	.loginPage("/login.html")//
-	                .loginProcessingUrl( "/j_spring_security_check" )
-					.usernameParameter("j_username")//
-					.passwordParameter("j_password")//
-	                .defaultSuccessUrl( "/" )
-	                .failureUrl("/login.html?error=INVALID_USER_OR_PASSWORD" )
-	                .permitAll()
-	            .and()
-	            .userDetailsService(this.userDetailsService)//
-	            .logout()
-	                .logoutUrl( "/j_spring_security_logout" )
-	                .logoutSuccessUrl( "/login.html" )
-	                .invalidateHttpSession( true )
-	            .and()
-	            .csrf().disable()
-	            ;
-	}
-
+        http
+            .authorizeRequests()
+                .antMatchers("/js/**", "/css/**", "/images/**", "fonts/**").permitAll()
+                .antMatchers("/index.jsp").fullyAuthenticated()
+                .antMatchers("/rs/**").fullyAuthenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login.html")//
+                    .loginProcessingUrl( "/j_spring_security_check" )
+                    .usernameParameter("j_username")//
+                    .passwordParameter("j_password")//
+                    .defaultSuccessUrl( "/" )
+                    .failureUrl("/login.html?error=INVALID_USER_OR_PASSWORD" )
+                    .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .userDetailsService(this.userDetailsService)//
+                .logout()
+                    .logoutUrl( "/j_spring_security_logout" )
+                    .logoutSuccessUrl( "/login.html" )
+                    .invalidateHttpSession( true )
+                .and()
+                .cors()
+                .and()
+                .csrf().disable()
+                ;
+    }
 }
