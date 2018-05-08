1ba8cc29cab0cdcd4b9330031f3a1cce588491a6
Robson <robsonmrsp@gmail.com>
2018-05-08 13:30:05 -0300

    Imports no SpringSecurityConfig.java

--- src/main/webapp/entries/common-files/appbase/src/main/java/br/com/app/config/SpringSecurityConfig.java
+++ src/main/webapp/entries/common-files/appbase/src/main/java/br/com/app/config/SpringSecurityConfig.java
@@ -5,15 +5,18 @@ import javax.inject.Inject;
 
 import javax.inject.Inject;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.google.common.collect.ImmutableList;
 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
